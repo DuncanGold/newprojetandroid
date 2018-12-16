3bd0c3e4bfe045e1d352a75fb439445b0998d9ba
Duncan G <duncang56@gmail.com>
2018-12-16 16:27:54 +0100

    changementsresults

--- testappli/app/src/main/java/com/example/duncan/testappli/ResultActivity.java
+++ testappli/app/src/main/java/com/example/duncan/testappli/ResultActivity.java
@@ -8,6 +8,7 @@ package com.example.duncan.testappli;
         import android.support.v7.app.AppCompatActivity;
         import android.transition.Slide;
         import android.view.View;
+        import android.widget.Button;
         import android.widget.TextView;
 
 public class ResultActivity extends Activity {
@@ -19,7 +20,7 @@ public class ResultActivity extends Activity {
         double score=0;
         TextView textresult = (TextView) findViewById(R.id.textResult);
         TextView scoreTotal = (TextView) findViewById(R.id.scoretotal);
-
+        Button butnxt= (Button)findViewById(R.id.btnNEXT);
 
         switch (getIntent().getIntExtra("typescore",1)) {
             case 1 : score = getIntent().getIntExtra("rightanswercount",0);
@@ -27,6 +28,11 @@ public class ResultActivity extends Activity {
                 break;
             case 2 : score = getIntent().getDoubleExtra("resultatshake",0);
             textresult.setText("Votre score : " + (int)score);
+            break;
+            case 3 : score = getIntent().getIntExtra("scoreslide",0);
+                textresult.setText("Votre score : " + (int)score);
+                butnxt.setText("Play Again");
+                break;
         }
 
 
@@ -51,6 +57,10 @@ public void NextGame(View view){
         case 2 :
             Intent appel1 = new Intent(getApplicationContext(), SlideActivity.class);
             startActivity(appel1);
+            break;
+        case 3 :
+            Intent appel2 = new Intent(getApplicationContext(), StartActivity.class);
+            startActivity(appel2);
             break;
     }
 }
